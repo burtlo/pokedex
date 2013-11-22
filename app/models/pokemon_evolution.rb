d6f4dedf9f4008d8b7ffcf20f4e8b4a419b10fb6
Franklin Webber <franklin.webber@gmail.com>
2013-11-22 13:32:26 -0700

    FIX finding pokemon evolutions by index not id

--- app/models/pokemon_evolution.rb
+++ app/models/pokemon_evolution.rb
@@ -2,6 +2,6 @@ class PokemonEvolution < ActiveRecord::Base
   belongs_to :pokemon
 
   def evolves_to
-    Pokemon.find(attributes["evolves_to"])
+    Pokemon.find_by_index(attributes["evolves_to"])
   end
 end
